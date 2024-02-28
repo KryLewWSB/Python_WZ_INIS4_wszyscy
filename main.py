ebc5f15881549a5ea1ef43e8a065ed22a0328b40
Krystian <krystian.lewandowski@teb.edu.pl>
2024-02-28 10:09:07 +0100

    7

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-wynikDzielenia = 15 / 5
-wynikDzielaniaCalkowitego = 15 // 5
-print(wynikDzielenia, " ", wynikDzielaniaCalkowitego)
-print(pow(2, 2), " ", 2 ** 2)
+print("Podaj roczny przychod (brutto): ")
+wprowadzonaWartosc = int(input())
+
+if(wprowadzonaWartosc < 120_000):
+    print(f"Wprowadziłeś przychod: {wprowadzonaWartosc} zł.\n"
+          f"Musisz zapłacić: {wprowadzonaWartosc * 0.12} zł.\n")
+else:
+    pierwszyProg = 120_000 * 0.12
+    drugiProg = (wprowadzonaWartosc - 120_000) * 0.32
+    print(f"Wprowadziłeś przychod: {wprowadzonaWartosc} zł.\n"
+          f"Musisz zapłacić w I progu podatkowym: {pierwszyProg} zł.\n"
+          f"Musisz zapłacić w II progu podatkowym: {drugiProg} zł.\n"
+          f"Ogół podatku: {pierwszyProg + drugiProg}\n")
+
+
+
+
