ae21eefd78f10530b1904cf32cdf1b6dad175b68
Krystian <krystian.lewandowski@teb.edu.pl>
2024-04-03 08:31:05 +0200

    03_04_24__1

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-print("Podaj roczny przychod (brutto): ")
-wprowadzonaWartosc = int(input())
 
-if(wprowadzonaWartosc < 120_000):
-    print(f"Wprowadziłeś przychod: {wprowadzonaWartosc} zł.\n"
-          f"Musisz zapłacić: {wprowadzonaWartosc * 0.12} zł.\n")
-else:
-    pierwszyProg = 120_000 * 0.12
-    drugiProg = (wprowadzonaWartosc - 120_000) * 0.32
-    print(f"Wprowadziłeś przychod: {wprowadzonaWartosc} zł.\n"
-          f"Musisz zapłacić w I progu podatkowym: {pierwszyProg} zł.\n"
-          f"Musisz zapłacić w II progu podatkowym: {drugiProg} zł.\n"
-          f"Ogół podatku: {pierwszyProg + drugiProg}\n")
+def dodaj():
+    print(2 + 5)
 
-
-
-
+dodaj()
